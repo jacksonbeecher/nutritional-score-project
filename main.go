61cac04ff87c06d4ec9b138eb8b4bedcc1a04738
Jackson Beecher <jbeecher@ashcom.com.au>
2022-08-09 15:10:23 +1000

    Generate nutrition value and score by hardcoded parameters.

--- main.go
+++ main.go
@@ -5,37 +5,16 @@ import (
 )
 
 func main() {
-
 	ns := GetNutritionalScore(NutritionalData{
 		Energy:              EnergyFromKcal(0),
 		Sugars:              SugarGram(10),
-		SaturatedFattyAcids: SaturatedFattyAcids(),
-		Sodium:              SodiumMilligram(),
-		Fruits:              FruitsPercent(),
-		Fibre:               FibreGram(),
-		Protein:             ProteinGram(),
+		SaturatedFattyAcids: SaturatedFattyAcids(2),
+		Sodium:              SodiumMilligram(500),
+		Fruits:              FruitsPercent(60),
+		Fibre:               FibreGram(4),
+		Protein:             ProteinGram(2),
 	}, Food)
+
 	fmt.Printf("Nurtitional Score %d\n", ns.Value)
-}
-
-func GetNutritionalScore(n NutritionalData, st ScoreType) NutritionalScore {
-
-	value := 0
-	positive := 0
-	negative := 0
-
-	if st != Water {
-		fruitPoints := n.Fruits.GetPoints()
-		fibrePoints := n.Fibre.GetPoints()
-
-		negative = n.Energy.GetPoints() + n.Sugars.GetPoints() + n.SaturatedFattyAcids.GetPoints() + n.Sodium.GetPoints()
-		positive = fruitPoints + fibrePoints + n.Protein.GetPoints()
-	}
-
-	return NutritionalScore{
-		Value:     value,
-		Positive:  positive,
-		Negative:  negative,
-		ScoreType: st,
-	}
+	fmt.Printf("NutriScore: %s\n", ns.GetNutriScore())
 }
